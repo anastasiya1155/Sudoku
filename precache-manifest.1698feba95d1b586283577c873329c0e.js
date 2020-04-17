41ebc0026ce0be14dca54d71a5478da03a39cfdc
ana1155 <anastasiya1155@gmail.com>
2020-04-17 17:28:37 +0300

    Updates

---
+++ precache-manifest.1698feba95d1b586283577c873329c0e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13ed5b84389f792e3c83eec4d460692c",
+    "url": "/sudoku/index.html"
+  },
+  {
+    "revision": "12739e53946c238fbe9d",
+    "url": "/sudoku/static/js/2.b3106d8d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/sudoku/static/js/2.b3106d8d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a3b4b2872ea5df543be1",
+    "url": "/sudoku/static/js/main.96ce0216.chunk.js"
+  },
+  {
+    "revision": "7f2f73783f1baa48b949",
+    "url": "/sudoku/static/js/runtime-main.e0cfc196.js"
+  }
+]);
